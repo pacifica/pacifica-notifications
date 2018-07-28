199f7ae1aee3cdfb5e2d2c796be386483de9254a
David Brown <dmlb2000@gmail.com>
2018-07-27 18:30:31 -0700

    fix testing

--- pacifica/notifications/test/eventmatch_test.py
+++ pacifica/notifications/test/eventmatch_test.py
@@ -2,46 +2,49 @@
 # -*- coding: utf-8 -*-
 """Test the example module."""
 from json import dumps
+import requests
 import cherrypy
 from cherrypy.test import helper
-from peewee import SqliteDatabase
 from pacifica.notifications.rest import orm, Root, error_page_default
 
 
 class EventMatchCPTest(helper.CPWebCase):
     """Test the EventMatch class."""
 
-    @classmethod
-    def setup_class(cls):
-        """Setup the class database."""
-        super(EventMatchCPTest, cls).setup_class()
-        cls._test_db = SqliteDatabase(':memory:')
-        orm.EventMatch.bind(cls._test_db)
+    HOST = '127.0.0.1'
+    PORT = 8070
+    url = 'http://{0}:{1}'.format(HOST, PORT)
+    headers = {'content-type': 'application/json'}
 
     @staticmethod
     def setup_server():
         """Bind tables to in memory db and start service."""
+        print('Entering setup_server().')
         orm.EventMatch.create_table()
         cherrypy.config.update({'error_page.default': error_page_default})
-        cherrypy.tree.mount(Root())
+        cherrypy.config.update('server.conf')
+        cherrypy.tree.mount(Root(), '/', 'server.conf')
+        print('Finished setup_server().')
 
     @classmethod
     def teardown_class(cls):
         """Destroy tables."""
+        print('Entering teardown_class().')
         super(EventMatchCPTest, cls).teardown_class()
-        cls._test_db.drop_tables([orm.EventMatch])
-        cls._test_db.close()
-        cls._test_db = None
+        orm.EventMatch.drop_table()
+        print('Finished teardown_class().')
 
     def test_create(self):
-        """Test the add method in example class."""
-        self.getPage(
-            '/eventmatch',
-            method='POST',
-            body=dumps({
+        """Test the create POST method in EventMatch."""
+        resp = requests.post(
+            '{}/eventmatch'.format(self.url),
+            data=dumps({
                 'name': 'testevent',
-                'jsonpath': 'foo[*].bar'
-            })
+                'jsonpath': 'foo[*].bar',
+                'target_url': 'http://example.com/callback'
+            }),
+            headers=self.headers
         )
-        self.assertStatus('200 OK')
-        self.assertHeader('Content-Type', 'application/json')
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue('Content-Type' in resp.headers)
+        self.assertEqual(resp.headers['Content-Type'], 'application/json')
