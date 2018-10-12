352818f1a4969746e3397e271a545112c0b0c03c
David Brown <dmlb2000@gmail.com>
2018-10-12 10:33:52 -0700

    add travis deploy and move tests to tests directory (#12)
    Signed-off-by: David Brown <dmlb2000@gmail.com>

--- tests/common_test.py
+++ tests/common_test.py
@@ -7,6 +7,7 @@ import cherrypy
 from cherrypy.test import helper
 from pacifica.notifications.orm import database_setup, EventMatch
 from pacifica.notifications.rest import Root, error_page_default
+from pacifica.notifications.globals import CHERRYPY_CONFIG
 
 
 def eventmatch_droptables(func):
@@ -31,8 +32,8 @@ class NotificationsCPTest(helper.CPWebCase):
     def setup_server():
         """Bind tables to in memory db and start service."""
         cherrypy.config.update({'error_page.default': error_page_default})
-        cherrypy.config.update('server.conf')
-        cherrypy.tree.mount(Root(), '/', 'server.conf')
+        cherrypy.config.update(CHERRYPY_CONFIG)
+        cherrypy.tree.mount(Root(), '/', CHERRYPY_CONFIG)
 
     def _create_eventmatch(self, **kwargs):
         """Create a test eventmatch and return resp."""
