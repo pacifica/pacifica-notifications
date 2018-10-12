cb387200d456bb946ad9cfe3cfcfacf2e2716d77
David Brown <dmlb2000@gmail.com>
2018-10-12 10:53:17 -0700

    add long description to setup.py and project url
    Signed-off-by: David Brown <dmlb2000@gmail.com>

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 """Setup and install the pacifica service."""
+from os import path
 try:  # pip version 9
     from pip.req import parse_requirements
 except ImportError:
@@ -17,6 +18,11 @@ setup(
     use_scm_version=True,
     setup_requires=['setuptools_scm'],
     description='Pacifica Notifications Service',
+    url='https://pypi.python.org/pypi/pacifica-notifications/',
+    long_description=open(path.join(
+        path.abspath(path.dirname(__file__)),
+        'README.md')).read(),
+    long_description_content_type='text/markdown',
     author='David Brown',
     author_email='dmlb2000@gmail.com',
     packages=find_packages(),
